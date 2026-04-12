195c2c5654ddd0a2ac54893cad5454d04aaaaa08
Devon Kirby <devonlkirby@gmail.com>
2026-04-12 14:02:02 -0500

    Updated to include more parameters for auth system

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,6 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 class Settings(BaseSettings):
     database_url: str
     secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
     anthropic_api_key: str
 
     class Config:
